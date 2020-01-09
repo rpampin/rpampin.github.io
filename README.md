47453ad17888540db3b6717dacf2f8037aacd91e
Rodrigo Pampin <pampin.r@fateryh.org.ar>
2020-01-09 07:57:20 -0300

    Test

--- README.md
+++
@@ -1,1 +0,0 @@
-# rpampin.github.io
---
+++ precache-manifest.b75c8ea2411282a108796909656b7e37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b837a587f794c696fb7c907176426975",
+    "url": "/rpampin.github.io/index.html"
+  },
+  {
+    "revision": "381614222ca901874543",
+    "url": "/rpampin.github.io/static/css/main.ba3703d5.chunk.css"
+  },
+  {
+    "revision": "24d0b85a52033ec40fab",
+    "url": "/rpampin.github.io/static/js/2.77e31003.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/rpampin.github.io/static/js/2.77e31003.chunk.js.LICENSE"
+  },
+  {
+    "revision": "381614222ca901874543",
+    "url": "/rpampin.github.io/static/js/main.63b6fd4b.chunk.js"
+  },
+  {
+    "revision": "f0cd81097e714cfbb038",
+    "url": "/rpampin.github.io/static/js/runtime-main.e197ff65.js"
+  }
+]);
